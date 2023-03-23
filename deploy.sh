112685fd55a0cfa82c8a5ee6d4c56ff1117cf582
LiDong <cnlidong@live.cn>
2023-03-23 15:33:01 +0800

    优化发布流程

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
-# set -x
+
 # 生成静态文件
-yarn  docs:build
-
-
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+echo 'xyhelper.cn' > CNAME
 
 git init
 git add -A
 git commit -m "deploy | $(date +'%Y-%m-%d %H:%M:%S')"
 
-echo "$(date +'%Y-%m-%d %H:%M:%S')|开始到github部署"
-git push -f git@github.com:xyhelper/xyhelper.github.io.git gh-pages
-echo "$(date +'%Y-%m-%d %H:%M:%S')|完成到github部署"
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-echo "发布完成~~~~~~~~~~~~~~~~~~~~~~~~~!"
-
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:xyhelper/xyhelper.github.io.git main:gh-pages
 
 cd -
