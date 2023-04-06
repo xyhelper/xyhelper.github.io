c6ef782e040c677193be5fa1a8631facad017cbf
LiDong <cnlidong@live.cn>
2023-04-06 08:53:40 +0800

    更新网页版相关信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    locales: {
-        // 键名是该语言所属的子路径
-        // 作为特例，默认语言可以使用 '/' 作为其路径。
-        '/': {
-          lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
-          title: 'XYHELPER',
-          description: '免费的AI助手'
-        }
-      },
-    themeConfig: {
-        logo: '/assets/img/logo.png',
-        sidebar: 'auto',
-        nav: [
-            { text: '首页', link: '/' },
-            { text: '使用教程', link: '/tutorial/' },
-            { text: '客户端下载', link: '/download/' },
-            { text: 'PLUS会员', link: '/plus/' },
-            { text: '开发者中心', link: '/developer/' },
-            { text: '专享接入点', link: '/access/' },
-            { text: '关于我们', link: '/about/' },
-          ],
-      }
-  }
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    "/": {
+      lang: "zh-CN", // 将会被设置为 <html> 的 lang 属性
+      title: "XYHELPER",
+      description: "免费的AI助手",
+    },
+  },
+  themeConfig: {
+    logo: "/assets/img/logo.png",
+    sidebar: "auto",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "使用教程", link: "/tutorial/" },
+      { text: "网页版", link: "/web/" },
+      { text: "客户端下载", link: "/download/" },
+      { text: "PLUS会员", link: "/plus/" },
+      { text: "开发者中心", link: "/developer/" },
+      { text: "专享接入点", link: "/access/" },
+      { text: "关于我们", link: "/about/" },
+    ],
+  },
+};
---
+++ docs/web/README.md
@@ -0,0 +1,66 @@
+# 网页版
+
+我们提供了开源的网页版，可以在浏览器中使用。
+
+项目地址：[GitHub](https://github.com/xyhelper/xyhelper-web)
+
+## 演示
+
+可访问 [https://chat.xyhelper.com](https://chat.xyhelper.com) 进行体验。
+
+手机端请扫描下方二维码：
+
+![https://chat.xyhelper.com](./qrcode.png)
+
+## 部署
+
+### Docker
+
+```bash
+docker run -d -p 8888:8080 --name xyhelper-web xyhelper/xyhelper-web --restart=always
+```
+
+部署完成后，访问 `http://localhost:8888` 即可。
+
+### Docker Compose
+
+创建 `docker-compose.yml` 文件，内容如下：
+
+```yaml
+version: "3.7"
+services:
+  xyhelper-web:
+    image: xyhelper/xyhelper-web
+    container_name: xyhelper-web
+    restart: always
+    ports:
+      - 8888:8080
+```
+
+在`docker-compose.yml`文件所在目录下执行如下命令：
+
+```bash
+docker-compose up -d
+```
+
+部署完成后，访问 `http://localhost:8888` 即可。
+
+## 更新
+
+### Docker
+
+```bash
+docker pull xyhelper/xyhelper-web
+docker restart xyhelper-web
+```
+
+### Docker Compose
+
+```bash
+docker-compose pull
+docker-compose up -d
+```
+
+## 注意事项
+
+网页版默认接入点是  `https://freechat.xyhelper.com` ，如果您服务器在国内，建议使用国内接入点 `https://freechat.lidong.xin` 。
